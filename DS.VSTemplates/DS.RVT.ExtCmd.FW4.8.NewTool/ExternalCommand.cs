cd1ff4c13947ffad1347d591b84d25c99b53357f
Данил Сараев <leonardo147wot@gmail.com>
2022-12-24 20:42:00 +0300

    projects added

---
+++ DS.VSTemplates/DS.RVT.ExtCmd.FW4.8.NewTool/ExternalCommand.cs
@@ -0,0 +1,18 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+
+namespace $safeprojectname$
+{
+    [Transaction(TransactionMode.Manual)]
+    public class ExternalCommand : IExternalCommand
+    {
+        public Autodesk.Revit.UI.Result Execute(ExternalCommandData revit,
+           ref string message, ElementSet elements)
+        {
+            TaskDialog.Show("Revit", "Hello World");
+            return Autodesk.Revit.UI.Result.Succeeded;
+        }
+    }
+}
---
+++ DS.VSTemplates/DS.RevitCmd.Template/ExternalCommand.cs
@@ -0,0 +1,23 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+
+namespace DS.RevitCmd.Template
+{
+    [Transaction(TransactionMode.Manual)]
+    public class ExternalCommand : IExternalCommand
+    {
+        public Autodesk.Revit.UI.Result Execute(ExternalCommandData commandData,
+           ref string message, ElementSet elements)
+        {
+            UIApplication uiApp = commandData.Application;
+            Autodesk.Revit.ApplicationServices.Application app = uiApp.Application;
+
+            UIDocument uiDoc = uiApp.ActiveUIDocument;
+            Document doc = uiApp.ActiveUIDocument.Document;
+
+            return Autodesk.Revit.UI.Result.Succeeded;
+        }
+    }
+}
